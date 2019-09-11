47f3aa42c5d46e4cae14e9ac7e039e219fe78037
hiddenratel <marek@woloszczak.eu>
2019-09-11 13:34:56 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,11 +25,19 @@ export class AppComponent  {
     jakisChar.description = "Postac Lukasza";
     jakisChar.attributes = new Array<Attribute>();
     jakisChar.attributes.push(new Attribute("WS",20,5,25))
-    jakisChar.attributes.push(new Attribute("BS",20,10,30))
+    jakisChar.attributes.push(new Attribute("BS",20,10,30));
+    
+        let third = new Character();
+    third.name = "Otto";
+    third.description = "Postac Lukasza";
+    third.attributes = new Array<Attribute>();
+    third.attributes.push(new Attribute("WS",20,5,25));
+    third.attributes.push(new Attribute("BS",20,10,30));
 
     this.characters = new Array<Character>();
     this.characters.push(firstChar);
     this.characters.push(jakisChar);
+    this.characters.push(third);
   }
 }
 
